4e5546ce38de8d672ec5b91ff162b25025f1c59c
Almin Iriskic <almin.iriskic@easelink.com>
2022-06-27 10:00:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Debouncer
+=========
+
+Simple debouncer that is based on the algorithm by [Kuhn].
+
+
+[Kuhn]: https://www.kennethkuhn.com/electronics/debounce.c
+[Greensted]: http://www.labbookpages.co.uk/electronics/debounce.html#soft
+[Ganssle]: http://www.ganssle.com/debouncing-pt2.htm
+[Hackaday]: https://hackaday.com/2015/12/10/embed-with-elliot-debounce-your-noisy-buttons-part-ii/
---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+pub struct Debouncer {
+    state: bool,
+    integrator: u8,
+    limit: u8,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum DebouncerResult {
+    State(bool),
+    Transition(bool),
+}
+
+impl Debouncer {
+    pub fn new(limit: u8) -> Debouncer {
+        Debouncer {
+            state: false,
+            integrator: 0,
+            limit,
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.state = false;
+        self.integrator = 0;
+    }
+
+    pub fn update(&mut self, pressed: bool) -> DebouncerResult {
+        if !pressed {
+            self.integrator = self.integrator.saturating_sub(1);
+        } else if self.integrator < self.limit {
+            self.integrator += 1;
+        }
+
+        let old_state = self.state;
+        if self.integrator == 0 {
+            self.state = false;
+        } else if self.integrator >= self.limit {
+            self.state = true;
+        }
+
+        if self.state != old_state {
+            DebouncerResult::Transition(self.state)
+        } else {
+            DebouncerResult::State(self.state)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut d = Debouncer::new(3);
+        assert_eq!(d.update(true), DebouncerResult::State(false));
+        assert_eq!(d.update(true), DebouncerResult::State(false));
+        assert_eq!(d.update(true), DebouncerResult::Transition(true));
+        assert_eq!(d.update(false), DebouncerResult::State(true));
+        assert_eq!(d.update(false), DebouncerResult::State(true));
+        assert_eq!(d.update(false), DebouncerResult::Transition(false));
+    }
+}
